f895a12f6cfd3d176828e5f069ad6923ff1567ab
kimdongwook <dongwook.kim@trigem.co.kr>
2024-10-16 08:44:23 +0900

    첫 커밋

---
+++ src/main/java/org/example/springweb1/Springweb1Application.java
@@ -0,0 +1,13 @@
+package org.example.springweb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springweb1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springweb1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springweb1/Springweb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springweb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springweb1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
